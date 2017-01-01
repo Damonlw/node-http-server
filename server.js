3653f468e8edbd11516e68c517e4bd8e309c8dba
Damon <Damon@MBP.local>
2017-01-01 20:26:24 +0800

    First

---
+++ server.js
@@ -0,0 +1,26 @@
+
+var http = require('http');
+var url = require('url');
+
+function start(route, handle){
+	http.createServer(function(request, response){
+		var postData = '';
+		var pathname = url.parse(request.url).pathname;
+		console.log('Rquest for' + pathname + ' received');
+
+		request.setEncoding('utf8')
+
+		request.addListener('data', function(postDataChunk){
+			postData += postDataChunk;
+			console.log("Received POST data chunk '"+ postDataChunk + "'.");
+		});
+		request.addListener('end', function(){
+			route(handle, pathname, response, postData)
+		});
+
+	}).listen(8888)
+}
+
+console.log('Server has started.');
+
+exports.start = start
